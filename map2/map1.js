5337ca83367c73a32e40b001133163b41d355cb1
User1 <user@example.com>
2020-04-16 08:27:12 -0500

    map2 update

--- map2/map1.js
+++
@@ -1,3 +0,0 @@
-/* global L */
-var map2 = L.map('map of Louisiana').setView([30.01, -90.00], 4)
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map2020)
---
+++ map2/map2.js
@@ -0,0 +1,15 @@
+/* global L */
+var LA = L.map('map2020').setView([30.415484, -91.185157], 9)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(LA)
+L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
+  layers: 'nexrad-n0r-900913',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+}).addTo(LA)
+L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
+  layers: 'warnings_c',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+}).addTo(LA)
