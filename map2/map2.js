0c2898ac35453d705754ddceffac9416dc232d42
User1 <user@example.com>
2020-04-16 08:30:30 -0500

    map2 update

--- map2/map2.js
+++ map2/map2.js
@@ -1,13 +1,13 @@
 /* global L */
 var LA = L.map('map2020').setView([30.415484, -91.185157], 9)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(LA)
-L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
+L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
   layers: 'nexrad-n0r-900913',
   format: 'image/png',
   transparent: true,
   attribution: 'NOAA, Iowa State University'
 }).addTo(LA)
-L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
+L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
   layers: 'warnings_c',
   format: 'image/png',
   transparent: true,
