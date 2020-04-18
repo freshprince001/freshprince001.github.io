3eecba6b9d894f7bc1994b2314f7960657a384f1
User1 <user@example.com>
2020-04-18 15:28:20 -0500

    map1 update

--- map1/map_Louisiana.js
+++ map1/map_Louisiana.js
@@ -1,6 +1,6 @@
 /* global L */
-var LA = L.map('map2020').setView([30.415484, -91.185157], 9)
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(LA)
+var LA = L.map('map2020').setView([30.300630, -90.840254], 9)
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}').addTo(LA)
 var LSU = L.marker([30.411846, -91.183894]).addTo(LA)
 var EBRP = L.polygon([
   [30.721228, -90.848330],
@@ -10,3 +10,12 @@ var EBRP = L.polygon([
 ]).addTo(LA)
 EBRP.bindPopup('This is East Baton Rouge Parish')
 LSU.bindPopup('This is the LSU campus')
+var latlngs = [
+  [30.427008, -91.168543],
+  [30.420877, -91.119310],
+  [30.307133, -90.994101],
+  [30.198674, -90.937545],
+  [29.944521, -90.074425]
+]
+var ROUTE = L.polyline(latlngs, { color: 'green' }).addTo(LA)
+ROUTE.bindPopup('This is the route from Baton Rouge to New Orleans')
