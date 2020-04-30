09d6ab16df24ec9d2f2689025b7d1c2cb9af1fe7
User1 <user@example.com>
2020-04-30 08:47:56 -0500

    map5 second test

--- google-map/map5.js
+++ google-map/map5.js
@@ -1,33 +1,31 @@
 /* global google */
 var map5 = new google.maps.Map(document.getElementById('map'), {
   zoom: 4,
-  center: { lat: 39, lng: -98 }
-})
+  center: { lat: 39, lng: -98 },
   mapTypeId: 'terrain',
-  gestureHandling: 'greedy',
-  map5.data.loadGeoJson('https://geog4046.github.io/portfolio/data/us_state_demographics_ESRI_2010A.geojson')
-  map5.data.setStyle({
-    fillColor: 'blue',
-    strokeColor: 'blue'
-  })
-  var style1 = function (feature) {
-    var asian = feature.getProperty('ASIAN')
-    var statecolor = 'red'
-    if (asian < 300000) {
-      statecolor = 'yellow'
-    }
-    return {
-      fillColor: statecolor,
-      strokeColor: statecolor
-    }
+  gestureHandling: 'greedy'
+})
+map5.data.loadGeoJson('https://geog4046.github.io/portfolio/data/us_state_demographics_ESRI_2010A.geojson')
+var style1 = function (feature) {
+  var age = feature.getProperty('MED_AGE')
+  var color = 'olive'
+  if (age < 38) {
+    color = 'green'
   }
-  map5.data.setStyle(style1)
-  var popup = new google.maps.InfoWindow()
-  map5.data.addListener('click', function (event) {
-  popup.setOptions({
-    content: event.feature.getProperty('STATE_NAME'),
+  return {
+    fillColor: color,
+    strokeColor: color,
+    fillOpacity: 0.4,
+    clickable: true,
+    strokeWeight: 2
+  }
+}
+map5.data.setStyle(style1)
+var infowindow = new google.maps.InfoWindow()
+map5.data.addListener('click', function (event) {
+  infowindow.setOptions({
+    content: 'The median age of ' + event.feature.getProperty('STATE_NAME') + ' is ' + event.feature.getProperty('MED_AGE') + '<br>National median: 38',
     position: event.latLng
   })
   infowindow.open(map5)
 })
-content: 'The median numbers of ' + event.feature.getProperty('STATE_NAME') + ' is ' + event.feature.getProperty('ASIAN') + '<br>National mean: 293063',
