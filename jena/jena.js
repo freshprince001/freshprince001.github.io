41859fa732ad55e336dea03a6d79091057197850
Yuan <xprince737@gmail.com>
2020-12-31 20:17:02 -0600

    1

--- jena/jena.js
+++ jena/jena.js
@@ -1,9 +1,26 @@
-var jenamap = L.map('jena').setView([51.505, -0.09], 13)
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={pk.eyJ1IjoiZnJlc2hwcmluY2UwMDEiLCJhIjoiY2tqZG4xczR6MTZ3MzJzbWFjOWtoZ2xrZSJ9.0iQE0Rve4OOBmtfk34cvQQ}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+var mymap = L.map('mapid').setView([51.505, -0.09], 2);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiZnJlc2hwcmluY2UwMDEiLCJhIjoiY2tqZG4xczR6MTZ3MzJzbWFjOWtoZ2xrZSJ9.0iQE0Rve4OOBmtfk34cvQQ'
-}).addTo(jena);
+    id: 'mapbox.streets',
+    accessToken: 'ADD MAPBOX ACCESS TOKEN HERE'
+}).addTo(mymap);
+
+marker1 = L.marker([51.505, -0.09]).addTo(mymap);
+marker2 = L.marker([40.743720, -73.822030]).addTo(mymap);
+marker3 = L.marker([39.760979, -84.192200]).addTo(mymap);
+
+function testFunction() {
+  mymap.closePopup();
+  marker1.bindPopup("<b>Spiderman</b><br>New York, USA").openPopup();
+}
+
+function testFunction2() {
+  mymap.closePopup();
+  marker2.bindPopup("<b>Hulk</b><br>Ohio, USA").openPopup();
+}
+
+function testFunction3() {
+  mymap.closePopup();
+  marker3.bindPopup("<b>Wolverine</b><br>Cold Lake, Alberta, Canada").openPopup();
+}
