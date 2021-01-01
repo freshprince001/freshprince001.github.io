e92450d520214d665587a37d732e53a1780eaf80
Yuan <xprince737@gmail.com>
2020-12-31 19:55:53 -0600

    1

--- mapofjena/jena.js
+++ mapofjena/jena.js
@@ -1,6 +1,17 @@
 /* global L jQuery */
-var map3 = L.map('map3').setView([52.00, -100.00], 3)
-var statesLayerObject = L.layerGroup().addTo(map3)
+var Jena = L.map('mapofjena').setView([52.00, -100.00], 3)
+var statesLayerObject = L.layerGroup().addTo(mapofjena)
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZnJlc2hwcmluY2UwMDEiLCJhIjoiY2tqZGxzNGZlMTY0bzMycXE5dnRkZmptNSJ9.i9XB2Mjnqog6nFIs888P3A'
+}).addTo(mapofjena);
+
+
 var basemapsObject = {
   Streets: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}').addTo(map3),
   GrayCanvas: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}').addTo(map3)
