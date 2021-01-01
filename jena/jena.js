f919a235c005c46d3e5b297e4f2817bff943f39c
Yuan <xprince737@gmail.com>
2020-12-31 20:07:04 -0600

    jena2

--- jena/jena.js
+++ jena/jena.js
@@ -1,3 +1,4 @@
+var jenamap = L.map('jena').setView([51.505, -0.09], 13)
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -5,4 +6,4 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiZnJlc2hwcmluY2UwMDEiLCJhIjoiY2tqZGxzNGZlMTY0bzMycXE5dnRkZmptNSJ9.i9XB2Mjnqog6nFIs888P3A'
-}).addTo(Jena);
+}).addTo(jena);
